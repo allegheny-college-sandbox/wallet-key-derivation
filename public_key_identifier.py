cae8ba03c6fd1ebd95fcc00327862ec97fc62471
Batbayasgalan Bat-Amgalan <baysaa@Batbayasgalans-MacBook-Pro.local>
2023-06-04 21:36:47 -0400

    Update

--- public_key_identifier.py
+++ public_key_identifier.py
@@ -1,10 +1,11 @@
 import requests
 import os
-
+import json
 data = os.getenv('GITHUB_CONTEXT')
 
 
 if data:
+    data_json = json.loads(data)
     username = data["triggering_actor"]
     x = requests.get(f'https://github.com/{username}.keys', timeout=10)
     print(f"Request came back with {x.status_code} code")
